d6f71b37ef910bbbb56927c662f207b047be9416
baltasarq <baltasarq@gmail.com>
2016-11-02 10:01:49 +0100

    Alpha

--- app/src/main/java/com/baltasarq/listacompra2/View/MainActivity.java
+++ app/src/main/java/com/baltasarq/listacompra2/View/MainActivity.java
@@ -1,8 +1,11 @@
 package com.baltasarq.listacompra2.View;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -14,6 +17,9 @@ import com.baltasarq.listacompra2.R;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected static final int CODIGO_EDICION_ITEM = 100;
+    protected static final int CODIGO_ADICION_ITEM = 102;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,16 +41,62 @@ public class MainActivity extends AppCompatActivity {
         btInserta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent subActividad = new Intent( MainActivity.this, ItemEditionActivity.class );
 
+                subActividad.putExtra( "nombre", "" );
+                subActividad.putExtra( "cantidad", 1 );
+                MainActivity.this.startActivityForResult( subActividad, CODIGO_ADICION_ITEM );
+            }
+        });
+
+        // Modifica
+        lvLista.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent subActividad = new Intent( MainActivity.this, ItemEditionActivity.class );
+                Item item = MainActivity.this.adaptadorItems.getItem( i );
+
+                subActividad.putExtra( "nombre", item.getNombre() );
+                subActividad.putExtra( "cantidad", item.getNum() );
+                subActividad.putExtra( "pos", i );
+                MainActivity.this.startActivityForResult( subActividad, CODIGO_EDICION_ITEM );
+
+                return true;
             }
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if ( requestCode == CODIGO_ADICION_ITEM
+          && resultCode == Activity.RESULT_OK )
+        {
+            Item item = new Item( data.getExtras().getString( "nombre").toString() );
+            item.setNum( data.getExtras().getInt( "cantidad" ) );
+            this.adaptadorItems.add( item );
+            this.updateStatus();
+        }
+
+        if ( requestCode == CODIGO_EDICION_ITEM
+          && resultCode == Activity.RESULT_OK )
+        {
+            int pos = data.getExtras().getInt( "pos" );
+            Item item = new Item( data.getExtras().getString( "nombre").toString() );
+
+            item.setNum( data.getExtras().getInt( "cantidad" ) );
+            this.items.set( pos, item );
+            this.adaptadorItems.notifyDataSetChanged();
+        }
+
+        return;
+    }
+
     private void updateStatus()
     {
-        TextView edNum = (TextView) this.findViewById( R.id.edNum );
+        TextView lblNum = (TextView) this.findViewById( R.id.lblNum );
 
-        edNum.setText( this.adaptadorItems.getCount() );
+        lblNum.setText( Integer.toString( this.adaptadorItems.getCount() ) );
     }
 
     private ArrayAdapter<Item> adaptadorItems;
